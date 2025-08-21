171669055b9f18fb03d4cf6f6a70f0e98d8b8dc6
sanskar <sanskar.jain@dataflask.com>
2025-08-21 12:04:03 +0530

    QE

--- src/routes/qeRoutes.js
+++ src/routes/qeRoutes.js
@@ -12,7 +12,7 @@ import {
 const router = express.Router();
 
 router.get("/qe", verifyToken, listAllQeProjects);
-router.post("/qe", verifyToken, createQeProject);
+router.post("/qe", verifyToken, upload, createQeProject);
 router.delete("/qe/:id", verifyToken, deleteQeProjectData);
 router.get("/qe/:id", verifyToken, getQeProjectById);
 router.patch("/qe/:id", verifyToken, updateQeProject); // <-- New route--------------------
